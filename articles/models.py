21d1baebf03340a262c73888358bb13b23dd3ada
andrew <goluschkin.a@yandex.by>
2019-09-06 12:31:28 +0300

    Add article model and register it on admin page

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('articles',args=[str(self.id)])
