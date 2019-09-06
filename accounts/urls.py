eeb2f8542b72490d3488771d977ed3e8183f97ac
andrew <goluschkin.a@yandex.by>
2019-09-06 12:50:54 +0300

    added allauth packet

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import SignUpView
-from django.urls import include
 
 
 urlpatterns = [
     path('signup/',SignUpView.as_view(),name='signup'),
-    path('auth/',include('social_django.urls',namespace='social')),
 ]
