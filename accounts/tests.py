98124d74596354579791621283e88efaed40ef70
andrew <goluschkin.a@yandex.by>
2019-09-06 12:36:37 +0300

    added accounts tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+class SignUpTests(TestCase):
+    username = 'testuser'
+    email = 'email@.com'
+
+    def test_signup_page_status_code(self):
+        res = self.client.get('/accounts/signup/')
+        self.assertEqual(res.status_code,200)
+
+    def test_view_uses_correct_template(self):
+        res = self.client.get(reverse('signup'))
+        self.assertEqual(res.status_code,200)
+        self.assertTemplateUsed(res,'signup.html')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            self.username,
+            self.email
+        )
+        self.assertEqual(get_user_model().objects.all().count(),1)
+        self.assertEqual(
+            get_user_model().objects.all()[0].username,
+            self.username
+        )
+        self.assertEqual(
+            get_user_model().objects.all()[0].email,
+            self.email
+        )
