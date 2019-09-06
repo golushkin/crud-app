a2ebd4b0c5c239068859173bf0f7560fbf3aa86a
andrew <goluschkin.a@yandex.by>
2019-09-06 12:45:01 +0300

    added google login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import SignUpView
+from django.urls import include
+
 
 urlpatterns = [
     path('signup/',SignUpView.as_view(),name='signup'),
+    path('auth/',include('social_django.urls',namespace='social')),
 ]
