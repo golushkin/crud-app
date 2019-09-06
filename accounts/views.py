8ea8adf92d43d946e390fd61036c14b8261cdaee
andrew <goluschkin.a@yandex.by>
2019-09-06 12:23:30 +0300

    added view signup, write templates for login/logout and signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import CutomUserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+class SignUpView(CreateView):
+    form_class = CutomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('home')
