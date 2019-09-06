52e5029ed0951c17e3874e4d863e5c27641cd820
andrew <goluschkin.a@yandex.by>
2019-09-06 12:30:10 +0300

    rewrite signup, added messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,17 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CutomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
         model = CustomUser
-        fields = ('username','email',)
+        fields = ('username','email','password1','password2')
+
+    def save(self,commit=True):
+        user = super(CutomUserCreationForm,self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 
 class CutomUserChangeForm(UserChangeForm):
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,22 @@
-from django.views.generic.edit import CreateView
 from .forms import CutomUserCreationForm
-from django.urls import reverse_lazy
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-class SignUpView(CreateView):
-    form_class = CutomUserCreationForm
-    template_name = 'signup.html'
-    success_url = reverse_lazy('home')
+def register(request):
+    if request.method == "POST":
+        form = CutomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            messages.success(request,'You created an account')
+            return redirect("home")
+        else:
+            for field in form:
+                for msg in field.errors:
+                    messages.error(request,'Error: {}'.format(msg))
+    form = CutomUserCreationForm()
+    return render(request = request,
+                  template_name = "signup.html",
+                  context={"form":form})
