c72573cc92b75a1c08d39d442ac5321bddb27b81
andrew <goluschkin.a@yandex.by>
2019-09-06 12:25:31 +0300

    update CustomUser model, add error_message and unique attr

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,14 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     date = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(
+        unique=True,
+        blank= False,
+        max_length=200,
+        error_messages={
+            "unique":"User with this email already exist",
+        }
+    )
 
     def __str__(self):
         return self.username
